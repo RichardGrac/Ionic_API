e5dbd74ab40946878815841d90ba1bf465e43c09
Ricardo Garcia <Richard_Grac@hotmail.com>
2018-03-05 12:02:31 -0600

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// Para utilizar la conexión anteriormente definida (en app.module.ts) dentro de home.ts
+import { Proveedor1Provider } from '../../providers/proveedor1/proveedor1';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  users
 
+  constructor(public navCtrl: NavController, public proveedor: Proveedor1Provider) {}
+
+  ionViewDidLoad(){
+    // subscribe := rescatamos los datos (en data - que sería el json) y los pasamos a una variable propia
+    // que contenga todos los datos (usuarios)
+    this.proveedor.getData()
+    .subscribe(
+      (data) => {this.users = data;}, 
+      //generamos error en caso de que ocurra
+      (error) => {console.log(error);}
+    )
   }
 
 }
